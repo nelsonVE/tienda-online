3176aa464efccad9642ec1644ddb555e9c245713
Nelson <nelsonmggomes@gmail.com>
2019-05-17 01:17:23 -0400

    Implementados roles

---
+++ app/Providers/RolServiceProvider.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class RolServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        //
+    }
+}
--- app/Rol.php
+++ app/Rol.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Rol extends Model
 {
-    //
+    protected $fillable = [
+        'nombre', 'descripcion'
+    ];
 }
